ea89688981acc8f6463c3a1caae55baf788e970e
unknown <dng5@P856112.nist.gov>
2014-05-14 16:46:37 -0400

    Test
    Just checking to see that changing code in one branch still leaves it in
    the master

--- ImagingModel3.py
+++ ImagingModel3.py
@@ -31,38 +31,3 @@ trange = np.arange(0,tfinal,dt)
 Inotrange = np.exp(np.arange(-2, 2, 0.5))
 num = zeros([zrange.size])
 
-
-def n(x):
-        return exp(-(x-zfinal/2)**2/(5.0e-10)) #approx. atom density
-        
-num[:] = ([n(z) for z in zrange])
-numsum = np.sum(num*dz)
-num = num*6.0e12/numsum
-    
-od = 6.0e12*A
-
-"define initial conditions"
-Inots = np.outer(Inotrange,np.ones(trange.size))
-Times = np.outer(np.ones(Inotrange.size),trange)   
-I = zeros([len(Inotrange),len(zrange)+1,len(trange)])
-D = zeros([len(Inotrange),len(zrange),len(trange)+1])
-I[:,0,:] = Inots
-Ifinal = zeros([len(Inotrange),len(trange)])
-Ifinaltot = zeros([len(Inotrange),len(trange)])
-rate = zeros([len(Inotrange)])
-
-for t in range(trange.size):
-  
-    for z in range(zrange.size):
-     
-        rate = (I[:,z,t])/(1+(D[:,z,t]**2)+I[:,z,t])
-        I[:,z+1,t]=I[:,z,t]-A*num[z]*rate*dz
-        D[:,z,t+1]=D[:,z,t]+B*rate*dt
-        
-    Ifinal[:,t] = I[:,len(zrange),t]
-    
-Ifinaltot = np.cumsum(Ifinal*dt,axis=1)/(Times+dt)
-od0 = -np.log(Ifinaltot/Inots)     
-od1 = -np.log(Ifinaltot/Inots) + (Inots-Ifinaltot)
-od2 = -np.log(Ifinaltot/Inots) + (Inots-Ifinaltot) - ((B*Times)**2)*(1/(Ifinaltot+1)-1/(Inots+1) + np.log((Ifinaltot+1)/(Inots+1)))/3        
-        
