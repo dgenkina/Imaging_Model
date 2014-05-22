7ff0f101e1e697360e10f14dec849acb762e6172
unknown <dng5@P856112.nist.gov>
2014-05-22 14:21:46 -0400

    Changed to choosing OD based on lowest average variance
    rather than finding the sum of the variances at the average of the
    isats between the three minima

--- compare.py
+++ compare.py
@@ -14,15 +14,16 @@ error75 = {}
 sdtdev75 = {}
 error40 = {}
 sdtdev40 = {}
+sdtdevTot={}
 Isatcounts = np.arange(1,80,0.5)
 
 ODrange = np.linspace(1.55,1.7,40)
-IsatAvg = np.empty([ODrange.size])
-SdtdevAvg = np.empty([ODrange.size])
+IsatMin = np.empty([ODrange.size])
+SdtdevT = np.empty([ODrange.size])
 
 for i in range(ODrange.size):
     ODinit = ODrange[i]
-    Simulate(ODinit,i)
+    #Simulate(ODinit,i)
     simul = np.load("SimulatedOD"+str(int(ODinit*1000))+".npz")
     for isat in Isatcounts:
         error100[isat] = np.interp(probeCl[times[2]]/times[2]/isat,simul['I0range'], simul['od0All'][:,500]) - Rod0Clean[times[2]]
@@ -31,16 +32,17 @@ for i in range(ODrange.size):
         sdtdev75[isat] = dot(error75[isat],error75[isat])
         error40[isat] = np.interp(probeCl[times[0]]/times[0]/isat,simul['I0range'], simul['od0All'][:,200]) - Rod0Clean[times[0]] 
         sdtdev40[isat] = dot(error40[isat],error40[isat])
+        sdtdevTot[isat]=sdtdev40[isat]+sdtdev75[isat]+sdtdev100[isat]        
         
-    IsatAvg[i] = ( min(sdtdev100, key=sdtdev100.get)+min(sdtdev75, key=sdtdev75.get)+min(sdtdev40, key=sdtdev40.get))/3.0
-    SdtdevAvg[i] = (np.interp(IsatAvg[i],Isatcounts,[sdtdev100[isat] for isat in Isatcounts])+np.interp(IsatAvg[i],Isatcounts,[sdtdev75[isat] for isat in Isatcounts])+np.interp(IsatAvg[i],Isatcounts,[sdtdev40[isat] for isat in Isatcounts]))/3.0 
+    IsatMin[i] = min(sdtdevTot, key=sdtdevTot.get)
+    SdtdevT[i] = np.interp(IsatMin[i],Isatcounts,[sdtdevTot[isat] for isat in Isatcounts])
     
-SdtdevMin = np.amin(SdtdevAvg)
-ODindex = int(np.where(SdtdevAvg==SdtdevMin)[0])
-IsatCounts = IsatAvg[ODindex]
+SdtdevMin = np.amin(SdtdevT)
+ODindex = int(np.where(SdtdevT==SdtdevMin)[0])
+IsatCounts = IsatMin[ODindex]
 ODbest = ODrange[ODindex]
 
-simul = np.load("SimulatedOD"+str(ODindex)+".npz")
+simul = np.load("SimulatedOD"+str(int(ODbest*1000))+".npz")
 for isat in Isatcounts:
     error100[isat] = np.interp(probeCl[times[2]]/times[2]/isat,simul['I0range'], simul['od0All'][:,500]) - Rod0Cl[times[2]]
     sdtdev100[isat] = dot(error100[isat],error100[isat])
@@ -48,15 +50,17 @@ for isat in Isatcounts:
     sdtdev75[isat] = dot(error75[isat],error75[isat])
     error40[isat] = np.interp(probeCl[times[0]]/times[0]/isat,simul['I0range'], simul['od0All'][:,200]) - Rod0Cl[times[0]]
     sdtdev40[isat] = dot(error40[isat],error40[isat])
+    sdtdevTot[isat]=sdtdev40[isat]+sdtdev75[isat]+sdtdev100[isat]    
 
 graph = plt.figure()
 fits = graph.add_subplot(1,1,1)
 fits.plot(Isatcounts,[sdtdev100[isat]for isat in Isatcounts], 'r-', linewidth = 2)
 fits.plot(Isatcounts,[sdtdev75[isat]for isat in Isatcounts], 'g-',linewidth = 2)
 fits.plot(Isatcounts,[sdtdev40[isat]for isat in Isatcounts], 'b-',linewidth = 2)
+fits.plot(Isatcounts,[sdtdevTot[isat]for isat in Isatcounts], 'd-',linewidth = 2)
 fits.set_xlabel('Isat [counts/us]', size=15)
-fits.set_ylabel('Standard deviation of OD' , size=15)
-fits.legend([fits.lines[i] for i in range(3)],['t = 100us','t = 75us', 't=40us'])
+fits.set_ylabel('Variance of OD' , size=15)
+fits.legend([fits.lines[i] for i in range(4)],['t = 100us','t = 75us', 't=40us', 'total'])
 fits.set_title('OD='+str(ODbest))
 show()
 
@@ -76,5 +80,6 @@ for t in range(len(timeIndexes)):
     odpanel.set_ylabel("OD", size=15)
 odpanel.legend((odpanel.lines[0],odpanel.lines[2],odpanel.lines[4]),('t=40us','t=75us','t=100us'))
 odpanel.set_xlim(0,3)
+odpanel.set_title('Isat = '+str(IsatCounts)+' counts/us')
 odfigure.show()
 
